6dad3aae96d42d088daac68a10844e79e90a9781
GrayHead <ss.zhuravlov@gmail.com>
2026-07-06 19:55:34 +0300

    lesson 4 functions

--- lessons/javascript/lessons/lesson4/main.js
+++ lessons/javascript/lessons/lesson4/main.js
@@ -1,1 +1,47 @@
+// let listOfItemsAdvanced = ['html advanced', 'css advanced', 'javascript advanced', 'mysql advanced', 'mongodb advanced', 'react advanced', 'angular advanced', 'node.js advanced'];
+//
+// function foobar(list) {
+//     for (const item of list) {
+//         document.write(`<p>${item}</p>`);
+//     }
+//
+// }
+//
+// // foobar() // list = undefined -> error
+//
+// foobar([11, 22, 33]); // list = [11,22,33]
+//
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// foobar(listOfItems); // list = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js']
+//
+//
+// let listOfNumbers = [11, 22, 33];
+// foobar(listOfNumbers); // list = listOfNumbers = [11,22,33]
+//
+// foobar(listOfItemsAdvanced);
+
+
+let user1 = {name: 'vasya', age: 31, status: false}
+let user2 = {name: 'petya', age: 30, status: true}
+let user3 = {name: 'kolya', age: 29, status: true}
+let user4 = {name: 'olya', age: 28, status: false}
+let user5 = {name: 'max', age: 30, status: true}
+let user6 = {name: 'anya', age: 31, status: false}
+let user7 = {name: 'oleg', age: 28, status: false}
+let user8 = {name: 'andrey', age: 29, status: true}
+let user9 = {name: 'masha', age: 30, status: true}
+let user10 = {name: 'abrikos', age: 2, status: false}
+let user11 = userGenerator('anna', 54, true)
+
+
+function userGenerator(customName, customAge, customStatus) {
+    let user = {name: customName, age: customAge, status: customStatus}
+    return user;
+}
+
+let data1 = userGenerator('vasya', 31, true);
+console.log(data1);
+console.log(userGenerator('petya', 32, false));
+
+
 
---
+++ lessons/javascript/lessons/lesson4/my_js_lib/lib.js
@@ -0,0 +1,16 @@
+function userGenerator(name, age, gender) {
+    let user = {name: name, age: age, gender: gender};
+    return user;
+}
+
+function calculator(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+function writeMessage(message) {
+    document.write(`<h1>${message}</h1>`)
+}
+
+
+
